366674db6b5d1c1552e879ccd2001aaff7ae9fcb
bagotaj <bagotaj@gmail.com>
2021-10-13 19:05:10 +0200

    Correcting appearance

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
 function displayWord(word) {
-    let op = 0;
+  let op = 0;
+  if (word.style.opacity === '1') {
+    return;
+  } else {
     let id = setInterval(duration, 100);
     function duration() {
-        if (op == 100) {
-            clearInterval(id);
-        } else {
-            op += 5;
+      if (op == 100) {
+        clearInterval(id);
+      } else {
+        op += 5;
 
-            word.style.opacity = op + "%";
-        }
+        word.style.opacity = op + '%';
+      }
     }
+  }
 }
